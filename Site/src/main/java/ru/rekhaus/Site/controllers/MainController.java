828c2f8aa3389b0220b919c3d602c97205c6f037
korneichikpavelvictorovich@gmail.com <korneichikpavelvictorovich@gmail.com>
2021-12-13 22:06:40 +0300

    Work Version

---
+++ Site/src/main/java/ru/rekhaus/Site/controllers/MainController.java
@@ -0,0 +1,29 @@
+package ru.rekhaus.Site.controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "Главная страница");
+        return "home";
+    }
+
+    @GetMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("title", "Страница про нас");
+        return "about";
+    }
+    @GetMapping("/blog")
+    public String blog(Model model) {
+        model.addAttribute("title", "Страница про нас");
+        return "blog-main";
+    }
+}
+
+
+
